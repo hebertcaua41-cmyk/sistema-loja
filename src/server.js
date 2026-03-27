bf2ea4b2f310de84111072c21a238c6274ba22e2
Hebert <seuemail@email.com>
2026-03-27 00:25:29 -0300

    Corrigindo caminho da pasta public

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
